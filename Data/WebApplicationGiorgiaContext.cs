b42c5728d504256823541c162f43bad059d2572a
giorgia220523 <cocisgiorgia@gmail.com>
2022-10-17 22:14:00 +0300

    Add project files.

---
+++ Data/WebApplicationGiorgiaContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationGiorgia.Models;
+
+namespace WebApplicationGiorgia.Data
+{
+    public class WebApplicationGiorgiaContext : DbContext
+    {
+        public WebApplicationGiorgiaContext (DbContextOptions<WebApplicationGiorgiaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplicationGiorgia.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<WebApplicationGiorgia.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Pages/Publishers/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationGiorgia.Data;
+using WebApplicationGiorgia.Models;
+
+namespace WebApplicationGiorgia.Pages.Publishers
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly WebApplicationGiorgia.Data.WebApplicationGiorgiaContext _context;
+
+        public DetailsModel(WebApplicationGiorgia.Data.WebApplicationGiorgiaContext context)
+        {
+            _context = context;
+        }
+
+      public Publisher Publisher { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Publisher == null)
+            {
+                return NotFound();
+            }
+
+            var publisher = await _context.Publisher.FirstOrDefaultAsync(m => m.ID == id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Publisher = publisher;
+            }
+            return Page();
+        }
+    }
+}
